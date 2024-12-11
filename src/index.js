8a3baf80419ade165c228e1602ee07734a0b1e0c
Alenia Allen <112526474+Alenia24@users.noreply.github.com>
2024-12-11 10:55:46 -0500

    Added search functionality using api

--- src/index.js
+++ src/index.js
@@ -1,10 +1,28 @@
+function updateWeather(response) {
+    console.log(response);
+    let temperatureElement = document.querySelector("#temperature");
+    let temperature = response.data.temperature.current;
+    temperatureElement.innerHTML = Math.round(temperature);
+
+    let cityElement = document.querySelector("#city");
+    cityElement.innerHTML = response.data.city; 
+}
+
+function searchCity(city) {
+    let apiKey = "fb2da4606e22fot4bcd3216b032ab697";
+    let apiUrl =
+      `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+    
+    axios.get(apiUrl).then(updateWeather);
+}
+
 function handleSearchSubmit(event) {
     event.preventDefault();
     let searchInput = document.querySelector("#search-form-input");
-    let cityElement = document.querySelector("#city");
-    cityElement.innerHTML = searchInput.value;
+    searchCity(searchInput.value)
 }
 
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
 
+searchCity("New York");
